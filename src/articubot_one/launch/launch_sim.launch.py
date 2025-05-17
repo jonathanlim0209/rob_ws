d007a66a3c6c0ff02df5f210fcf3fc3d4e51ae3e
Jonathan <jonathanlim0209@gmail.com>
2025-05-17 22:38:12 +0800

    changed the gazebo control yaml file so that only velocity control is used

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -8,6 +8,8 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 
 
@@ -25,11 +27,6 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
-    joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
 
     # twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     # twist_mux = Node(
@@ -39,14 +36,14 @@ def generate_launch_description():
     #         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
     #     )
 
-    # gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
-    # # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #                 launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-    #          )
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -71,17 +68,23 @@ def generate_launch_description():
     # Code for delaying a node (I haven't tested how effective it is)
     # 
     # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
+
     #
     # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+    event_handler=OnProcessExit(
+        target_action=spawn_entity,
+        on_exit=[joint_broad_spawner],
+    )
+    )
+    
     # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
 
@@ -89,10 +92,10 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        joystick,
+        #joystick,
         #twist_mux,
-        #gazebo,
+        gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        delayed_joint_broad_spawner,
+        delayed_diff_drive_spawner,
     ])
